e5656ddca4a8686a4a7e476e8b17a239fef90d36
Angel <matamorosfernandez@gmail.com>
2025-02-20 16:04:35 -0300

    Curso de Python 20-02-2025

---
+++ Ejemplos/variables.py
@@ -0,0 +1,20 @@
+# sintaxis para definir una variable
+# declaracion e inicializacion de variables
+edad = 28
+altura = 1.65
+pais = "Paraguay"
+
+# acceder a las variables
+print('Variables Inicales')
+print('Edad:', edad)
+print('Altura:', altura)
+print('Pais:', pais)
+
+# modificar el valor d euna variable
+edad = 30
+altura = 1.70
+pais = "Paraguay"
+print('Variables Modificados')
+print('Edad:', edad)
+print('Altura:', altura)
+print('Pais:', pais)
---
+++ Ejercicios/hello.py
@@ -0,0 +1,4 @@
+# Ejercicio de Presentacion con Python
+print("Nombre: Angel Sanabria")
+print("Edad: 28")
+print("Pais: Paraguay")

03b83e4700601988885691a43523f573042b9cb1
Angel <matamorosfernandez@gmail.com>
2025-02-24 16:05:48 -0300

    24/02/2025

---
+++ Ejemplos/cadena.py
@@ -0,0 +1,23 @@
+#manejo de cadenas 
+cadena1 = "Hola Mundo"
+cadena1 = "Adios"
+cadena2 = "Python es genial"
+cadena3 = '''Este es un ejemplo de 
+multiples lineas en cadena,
+lleva 3 comillas simples'''
+print(cadena1)
+print(cadena2)
+print(cadena3)
+
+#Manejo de indices en cadena
+
+cadena4 = 'Hola Mundo'
+print(cadena4)
+#-------------------------
+primer_caracter = cadena4[0]
+print(primer_caracter)
+
+#Inmutabilidad de una cadena
+cadena5 = "Hola MUndo"
+
+print(cadena5)
